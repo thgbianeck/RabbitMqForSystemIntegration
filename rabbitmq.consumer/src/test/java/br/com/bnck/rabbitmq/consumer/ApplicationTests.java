954ec4e9a8224dcadeb0b1c97d0c6c6dfa3c0e37
Thiago Moreira Bianeck <thgbianeck@gmail.com>
2022-07-07 20:36:02 -0300

    Initial commit

---
+++ rabbitmq.consumer/src/test/java/br/com/bnck/rabbitmq/consumer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bnck.rabbitmq.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
